241edafe2ea00f1b910042d6f9e10f0c3248fb50
Nortey Michael <norteymicheal91@gmail.com>
2025-02-09 01:50:12 +0000

    Update WhoWeAre component layout for improved responsiveness and spacing

--- src/components/whoweare.tsx
+++ src/components/whoweare.tsx
@@ -1,8 +1,8 @@
 export default function WhoWeAre() {
     return (
-        <div className="container text-center w-[90vw] py-[80px]">
+        <div className="max-w-[1200px] mx-auto text-center w-[90vw] pt-[80px] pb-[180px]">
             <h3 className="text-3xl font-bold mb-8">Who We Are</h3>
-            <p className="text-slate-800 text-lg lg:text-2xl">We're more than a fintech solution. Our mission is to empower individuals to transact globally with ease and confidence. Our platform is designed to make sending money trading crypto, and redeeming gift cards accessible, secure, and instant, all while offering top-notch support and transparent fees.</p>
+            <p className="text-slate-800 text-lg/relaxed lg:text-2xl/relaxed">We're more than a fintech solution. Our mission is to empower individuals to transact globally with ease and confidence. Our platform is designed to make sending money trading crypto, and redeeming gift cards accessible, secure, and instant, all while offering top-notch support and transparent fees.</p>
         </div>
     )
 }
